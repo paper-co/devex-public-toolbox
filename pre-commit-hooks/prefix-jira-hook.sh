895d9cfdaf2ba4f4f796711c8bf575f66f1c7a82
Geoffrey Bachelet <geoffrey@paper.co>
2023-04-12 16:29:17 -0400

    Fix prefix-jira-hook
    - Preserve newlines
    - Do not duplicate prefix

--- pre-commit-hooks/prefix-jira-hook.sh
+++ pre-commit-hooks/prefix-jira-hook.sh
@@ -4,16 +4,39 @@
 # below searches for JIRA issue key's. The issue key will
 # be extracted out of your branch name
 
+if [ -z "$1" ]; then
+  echo
+  echo "Usage: $0 <file-path>"
+  exit 1
+fi
+
 REGEX_ISSUE_ID="[a-zA-Z0-9,\.\_\-]+-[0-9]+"
 
 # Find current branch name
-BRANCH_NAME=$(git symbolic-ref --short HEAD)
+BRANCH_NAME=${BRANCH_NAME:-$(git symbolic-ref --short HEAD)}
 
 if [[ -z "$BRANCH_NAME" ]]; then
-    echo "No branch name... "; exit 1
+    echo "No branch name"
+    exit 1
 fi
 
 # Extract issue id from branch name
 ISSUE_ID=$(echo "$BRANCH_NAME" | grep -o -E "$REGEX_ISSUE_ID")
+PREFIX="$ISSUE_ID: "
 
-echo "$ISSUE_ID"': '$(cat "$1") > "$1"
+# Check if message already has issue prefix
+
+PREFIX_IN_COMMIT=$(grep -c "^$PREFIX" $1)
+
+if [[ $PREFIX_IN_COMMIT -ge 1 ]]; then
+  echo "Message already prefixed"
+  exit 0
+fi
+
+MESSAGE="$ISSUE_ID: $(cat $1)"
+
+if [ -z "$DEBUG" ]; then
+  echo "$MESSAGE" > "$1"
+else
+  echo "$MESSAGE"
+fi
